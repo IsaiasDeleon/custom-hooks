cc3ae3eb0968ae9d41bd8ff3a2ad5fcf5efc86df
IsaiasDeleon <deleonsalazaru@gmail.com>
2022-08-17 14:21:48 -0500

    Primer commit

---
+++ useTodos.js
@@ -0,0 +1,45 @@
+
+const initialState = []
+const init = () =>{
+    return JSON.parse(localStorage.getItem('todos')) || []
+}
+import { useReducer, useEffect } from "react"
+import { todoReducer } from "../08-useReducer/todoReducer"
+
+export const useTodo = () =>{
+
+    const [todos, dispatch] = useReducer( todoReducer, initialState, init);
+    useEffect(()=>{
+        localStorage.setItem('todos', JSON.stringify( todos ))
+    },[todos])
+
+    const handleNewTodo = ( todo ) =>{
+        const action = {
+            type:'Add Todo',
+            payload:todo
+        }
+        dispatch( action )
+    }
+
+    const heandleDeleteTodo = ( id ) =>{
+        dispatch({
+            type:'Remove Todo',
+            payload:id
+        })
+    }
+    const onToggleTodo = ( id ) =>{
+        dispatch({
+            type:'Toggle Todo',
+            payload:id
+        })
+    }
+    return {
+        handleNewTodo,
+        heandleDeleteTodo,
+        onToggleTodo,
+        todos,
+        todosCount: todos.length,
+        pendingTodosCount: todos.filter((todo)=> !todo.done).length
+    }
+
+}
